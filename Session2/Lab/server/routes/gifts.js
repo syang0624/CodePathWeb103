7b539fc920ec5a94d3408e29570a044aa1486955
syang0624 <steven.yang@uni.minerva.edu>
2024-09-26 16:46:17 -0700

    lab 2 done

--- Session2/Lab/server/routes/gifts.js
+++ Session2/Lab/server/routes/gifts.js
@@ -1,21 +1,20 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
+import GiftsController from "../controllers/gifts.js";
 
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from "url";
 
-import giftData from '../data/gifts.js'
+import giftData from "../data/gifts.js";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json(giftData)
-})
+router.get("/", GiftsController.getGifts);
 
-router.get('/:giftId', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
-})
+router.get("/:giftId", (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "../public/gift.html"));
+});
 
-export default router
+export default router;
--- Session2/Lab/server/server.js
+++ Session2/Lab/server/server.js
@@ -1,21 +1,22 @@
-import express from 'express'
-import giftsRouter from './routes/gifts.js'
+import express from "express";
+import "../server/config/dotenv.js";
+import giftsRouter from "./routes/gifts.js";
 
-const app = express()
+const app = express();
 
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+app.use("/public", express.static("./public"));
+app.use("/scripts", express.static("./public/scripts"));
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
-  })
+app.get("/", (req, res) => {
+    res.status(200).send(
+        '<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>'
+    );
+});
 
-app.use('/gifts', giftsRouter)
+app.use("/gifts", giftsRouter);
 
-const PORT = process.env.PORT || 3001
-    
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`)
-})
-
-
+    console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
