a555bfab267beecaa1ac9396c78e2c26ee1c7642
syang0624 <steven.yang@uni.minerva.edu>
2024-09-26 19:49:43 -0700

    update cnxn?

--- Session2/Lab/server/config/database.js
+++ Session2/Lab/server/config/database.js
@@ -1,12 +1,10 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
+dotenv.config({ path: "../.env" });
+
 const config = {
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
+    connectionString: process.env.DATABASE_PUBLIC_URL,
 };
 
 export const pool = new pg.Pool(config);
